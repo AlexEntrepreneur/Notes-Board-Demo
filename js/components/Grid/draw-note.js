79fe14e66f41ebb8aa1edde479d2ce556173a868
Alex Entrepreneur <AlexEntrepreneur@users.noreply.github.com>
2023-04-26 12:30:09 +0400

    Add Mobile Support For drawNote()

--- js/components/Grid/draw-note.js
+++ js/components/Grid/draw-note.js
@@ -9,21 +9,57 @@ export default function drawNote() {
   this.element.style.cursor = 'crosshair'
   toggleGridDraggable()
   
-  this.element.addEventListener('mousedown', function handleMouseDown(e) {
+  function handleMouseDown(e) {
     e.preventDefault()
-    const startPosition = { x: e.clientX, y: e.clientY }
-    window.addEventListener('mouseup', draw(this, handleMouseDown, startPosition, toggleGridDraggable))
-  })
+    const startPosition = {
+      x: e.clientX || e.changedTouches[0].clientX,
+      y: e.clientY || e.changedTouches[0].clientY,
+    }
+
+    if (e.type === "mousedown") {
+      window.addEventListener(
+        'mouseup',
+        draw(this, handleMouseDown, startPosition, toggleGridDraggable)
+      )
+    }
+    if (e.type === 'touchstart') {
+      window.addEventListener(
+        'touchend',
+        draw(this, handleMouseDown, startPosition, toggleGridDraggable)
+      )  
+    }
+  }
+  
+  this.element.addEventListener('mousedown', handleMouseDown)
+  this.element.addEventListener('touchstart', handleMouseDown)
   
   function draw(gridElement, handleMouseDown, startPosition, toggleGridDraggable) {
     return function handleMouseUp(e) {
+      e.preventDefault()
+      const endPosition = {
+        x: e.clientX || e.changedTouches[0].clientX,
+        y: e.clientY || e.changedTouches[0].clientY,
+      }
       const scrollCoords = { x: window.scrollX, y: window.scrollY }
-      const coords = addMinWidthHeightToCoords(snapCoordsToGrid(deltaToElementCoords(startPosition.x + scrollCoords.x, startPosition.y + scrollCoords.y, e.clientX + scrollCoords.x, e.clientY + scrollCoords.y), gridUnit), gridUnit)
+      const coords = addMinWidthHeightToCoords(
+        snapCoordsToGrid(
+          deltaToElementCoords(
+            startPosition.x + scrollCoords.x,
+            startPosition.y + scrollCoords.y,
+            endPosition.x + scrollCoords.x,
+            endPosition.y + scrollCoords.y
+          ),
+          gridUnit
+        ),
+        gridUnit
+      )
       
       globalDispatcher.dispatch(createNote({ id: generateId(), ...coords, text: '' }))
      
       gridElement.style.cursor = 'grab'
       toggleGridDraggable()
+      gridElement.removeEventListener('touchstart', handleMouseDown)
+      window.removeEventListener('touchend', handleMouseUp)
       gridElement.removeEventListener('mousedown', handleMouseDown)
       window.removeEventListener('mouseup', handleMouseUp)
     }
